e5eb6a66d7c636551dcae34c3e0186aa2a1b4fb2
unknown <levantuan6898@gmail.com>
2020-05-07 20:01:41 +0700

    add file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        JDBCConnectionPool pool = new JDBCConnectionPool("org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+                "sa", "secret");
+        Connection con = pool.checkOut();
+        pool.checkIn(con);
+    }
+}
